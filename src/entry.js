26e898c5d344576fe237dcc0595ccbf22e82bdc3
Zeff Svoboda <zeff@alteredconstants.com>
2016-11-25 18:11:36 -0800

    Create separate root router.

--- src/entry.js
+++ src/entry.js
@@ -1,21 +1,13 @@
 import 'source-map-support/register';
 import path from 'path';
 import Koa from 'koa';
-import Router from 'koa-router';
-import prettyPrint from 'koa-json';
 import Datastore from 'nedb-promise';
 import { connect } from 'camo';
-import apiRouter from './routes/api';
-import sampleDataRouter from './routes/sampleData';
+import router from './router';
 
 const datastoreDirPath = path.join(__dirname, '../.temp/datastores');
 
-const router = new Router();
-router.use('/api', apiRouter.routes(), apiRouter.allowedMethods());
-router.use('/sample_data', sampleDataRouter.routes());
-
 const app = new Koa();
-app.use(prettyPrint());
 app.use(router.routes());
 
 Promise.resolve()
